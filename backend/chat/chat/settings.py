4f36257dc9ec49346a378fdc8daf9af7b399203c
Pritam Debnath <debnathpritam2222@gmail.com>
2024-03-25 21:54:25 +0530

    push

--- backend/chat/chat/settings.py
+++ backend/chat/chat/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
+    'whitenoise.runserver_nostatic',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
@@ -53,6 +54,7 @@ MIDDLEWARE = [
     "django.middleware.common.CommonMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -99,14 +101,11 @@ CHANNEL_LAYERS = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'chatlite',
-        'USER': 'chatliteuser',
-        'PASSWORD': 'chatliteuser',
-        'HOST': '127.0.0.1',
+        'NAME': 'chatingDB',
+        'USER': 'chating_admin',
+        'PASSWORD': 'chating_admin',
+        'HOST': 'postgrechatingdb.cbiosacmcer5.eu-north-1.rds.amazonaws.com',
         'PORT': '5432',
-        'OPTIONS': {
-            'options': '-c search_path=newchatschema',
-        },
     }
 }
 
@@ -198,3 +197,20 @@ SIMPLE_JWT = {
     "SLIDING_TOKEN_OBTAIN_SERIALIZER": "rest_framework_simplejwt.serializers.TokenObtainSlidingSerializer",
     "SLIDING_TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSlidingSerializer"
 }   
+
+AWS_ACCESS_KEY_ID = 'AKIA4MTWIIDGWXB5QTVZ'
+AWS_SECRET_ACCESS_KEY = 'wNJW2zcuF9cudyzqoIzlFxGwd8PpNorbAboJLyJ0'
+AWS_STORAGE_BUCKET_NAME = 'bucketchatting'
+AWS_S3_REGION_NAME = 'eu-north-1'  # e.g. 'us-east-1'
+AWS_S3_SIGNATURE_NAME= 's3v4'
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+AWS_S3_VERIFY = True
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+# For serving static files from S3
+# STATIC_URL = f"https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/"
+# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+# For serving media files from S3
+# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
